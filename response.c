90570ee456fd63d9daf8449ff8597a462ffb5aab
trainking <yefulincode@gmail.com>
2020-09-03 19:50:09 +0800

    add respoese 2.xx

--- response.c
+++ response.c
@@ -10,6 +10,9 @@ void response_200(int client, char *result);
 void response_201(int client, char *result);
 void response_202(int client, char *result);
 void response_203(int client, char *result);
+void response_204(int client, char *result);
+void response_205(int client, char *result);
+void response_206(int client, char *result);
 void response_505(int client, char *result);
 
 // 100 Continue
@@ -81,6 +84,42 @@ void response_203(int client, char *result) {
     sprintf(result, "\r\n");
     send(client, result, strlen(result), 0);
     sprintf(result, "Non-Authoritative Information\r\n");
+    send(client, result, strlen(result), 0);
+}
+
+// 204 No Content 返回内容没有body
+void response_204(int client, char *result) {
+    sprintf(result, "HTTP/1.1 204 No Content\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "Content-type: text/html\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "No Content\r\n");
+    send(client, result, strlen(result), 0);
+}
+
+// 205 Reset Content 应重置
+void response_205(int client, char *result) {
+    sprintf(result, "HTTP/1.1 204 Reset Content\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "Content-type: text/html\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "Reset Content\r\n");
+    send(client, result, strlen(result), 0);
+}
+
+// 206 Partial Content 请求头里包含Range字段，读取部分
+void response_206(int client, char *result) {
+    sprintf(result, "HTTP/1.1 Partial Content\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "Content-type: text/html\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "\r\n");
+    send(client, result, strlen(result), 0);
+    sprintf(result, "Partial Content\r\n");
     send(client, result, strlen(result), 0);
 }
 
